7f44329e97e3af087eda1de6ee18a04343cde2c4
zoomdmartin02 <72090048+zoomdmartin02@users.noreply.github.com>
2021-01-15 22:52:21 -0600

    mmp2

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -20,7 +20,7 @@ let map = L.map("mapid", {
 //    accessToken: API_KEY
 //});
 
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
@@ -36,8 +36,51 @@ streets.addTo(map);
 //  radius: 100
 //}).addTo(map);
 
-L.circleMarker([34.0522, -118.2437], {
-  radius: 30,
-  color:  "black",
-  fillColor:  '#ffffa1'
-}).addTo(map);
+//L.circleMarker([34.0522, -118.2437], {
+//  radius: 30,
+//  color:  "black",
+//  fillColor:  '#ffffa1'
+//}).addTo(map);
+
+// An array containing each city's location, state, and population.
+let cities = [{
+  location: [40.7128, -74.0059],
+  city: "New York City",
+  state: "NY",
+  population: 8398748
+},
+{
+  location: [41.8781, -87.6298],
+  city: "Chicago",
+  state: "IL",
+  population: 2705994
+},
+{
+  location: [29.7604, -95.3698],
+  city: "Houston",
+  state: "TX",
+  population: 2325502
+},
+{
+  location: [34.0522, -118.2437],
+  city: "Los Angeles",
+  state: "CA",
+  population: 3990456
+},
+{
+  location: [33.4484, -112.0740],
+  city: "Phoenix",
+  state: "AZ",
+  population: 1660272
+}
+];
+
+// Loop through the cities array and create one marker for each city.
+cities.forEach(function(city) {
+  console.log(city)
+  L.circleMarker(city.location, {
+    radius: city.population/100000
+  })
+  .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <br> <h3> Population " + city.population.toLocaleString() + "</h3>")
+  .addTo(map);
+ });
