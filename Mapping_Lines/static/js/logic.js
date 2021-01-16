36e0f943f02817befc6df84b568073839b4be51c
zoomdmartin02 <72090048+zoomdmartin02@users.noreply.github.com>
2021-01-16 08:39:17 -0600

    13.4.3

--- Mapping_Lines/static/js/logic.js
+++ Mapping_Lines/static/js/logic.js
@@ -8,9 +8,9 @@ console.log("working");
 
 let map = L.map("mapid", {
     center: [
-      40.7, -94.5
+      37.6213, -122.3790
     ],
-    zoom: 4
+    zoom: 5
   });
 
 // We create the tile layer that will be the background of our map.
@@ -20,7 +20,7 @@ let map = L.map("mapid", {
 //    accessToken: API_KEY
 //});
 
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
@@ -37,37 +37,37 @@ streets.addTo(map);
 //}).addTo(map);
 
 // An array containing each city's location, state, and population.
-let cities = [{
-  location: [40.7128, -74.0059],
-  city: "New York City",
-  state: "NY",
-  population: 8398748
-},
-{
-  location: [41.8781, -87.6298],
-  city: "Chicago",
-  state: "IL",
-  population: 2705994
-},
-{
-  location: [29.7604, -95.3698],
-  city: "Houston",
-  state: "TX",
-  population: 2325502
-},
-{
-  location: [34.0522, -118.2437],
-  city: "Los Angeles",
-  state: "CA",
-  population: 3990456
-},
-{
-  location: [33.4484, -112.0740],
-  city: "Phoenix",
-  state: "AZ",
-  population: 1660272
-}
-];
+//let cities = [{
+//  location: [40.7128, -74.0059],
+//  city: "New York City",
+//  state: "NY",
+//  population: 8398748
+//},
+//{
+//  location: [41.8781, -87.6298],
+//  city: "Chicago",
+//  state: "IL",
+//  population: 2705994
+//},
+//{
+//  location: [29.7604, -95.3698],
+//  city: "Houston",
+//  state: "TX",
+//  population: 2325502
+//},
+//{
+//  location: [34.0522, -118.2437],
+//  city: "Los Angeles",
+//  state: "CA",
+//  population: 3990456
+//},
+//{
+//  location: [33.4484, -112.0740],
+//  city: "Phoenix",
+//  state: "AZ",
+//  population: 1660272
+//}
+//];
 
 //L.circleMarker([34.0522, -118.2437], {
 //  radius: 30,
@@ -76,14 +76,27 @@ let cities = [{
 //}).addTo(map);
 
 // Get data from cities.js
-let cityData = cities;
+//let cityData = cities;
 
 // Loop through the cities array and create one marker for each city.
-cityData.forEach(function(city) {
-  console.log(city)
-  L.circleMarker(city.location, {
-    radius: city.population/100000
-  })
-  .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
-.addTo(map);
-});
+//cityData.forEach(function(city) {
+//  console.log(city)
+//  L.circleMarker(city.location, {
+//    radius: city.population/100000
+//  })
+//  .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+//.addTo(map);
+//});
+
+// Coordinates for each point to be used in the line.
+let line = [
+  [33.9416, -118.4085],
+  [37.6213, -122.3790],
+  [40.7899, -111.9791],
+  [47.4502, -122.3088]
+];
+
+// Create a polyline using the line coordinates and make the line red.
+L.polyline(line, {
+  color: "yellow"
+}).addTo(map);
